159d04bcdfdf40d8ab15d03302232a8cd7881737
Max Smith <103558620+Max-Sepp@users.noreply.github.com>
2024-02-11 22:48:08 +0000

    using clap crate for cli args

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,33 @@ mod ignore_files;
 mod list_files;
 mod output;
 
-use std::env;
+use clap::{Parser, Subcommand};
+use output::{file_output::FileOutput, terminal_output::TerminalOutput, Output};
 
-use ignore_files::IgnoreFiles;
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum Commands {
+    File { output_path: String },
+    Terminal,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    if args.len() < 2 {
-        panic!("Did not enter output type")
+    let mut output: Box<dyn Output>;
+    match args.cmd {
+        Commands::File { output_path } => output = Box::new(FileOutput::new(&output_path)),
+        Commands::Terminal => output = Box::new(TerminalOutput::new()),
     }
 
-    // the first agrument is how the user wants it to be inputted into the system.
-    // the second argument is any associated information about the input e.g. file name to output the data to.
-    let mut output = output::new(&args[1], args[2..].to_vec());
-
     // a list of files to check against if the file should be ignored
-    let ignore_files = IgnoreFiles::new();
+    let ignore_files = ignore_files::IgnoreFiles::new();
 
     list_files::list_files(&mut output, &ignore_files);
 }
